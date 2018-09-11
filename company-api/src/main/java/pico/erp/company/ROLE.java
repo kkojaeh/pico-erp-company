8033c258bc4438a081ff51a748a8825c2203141e
Ethan <kkojaeh@gmail.com>
2018-09-11 13:43:54 +0900

    메뉴 제거

--- company-api/src/main/java/pico/erp/company/ROLE.java
+++ company-api/src/main/java/pico/erp/company/ROLE.java
@@ -1,27 +1,17 @@
 package pico.erp.company;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import pico.erp.shared.data.Menu;
 import pico.erp.shared.data.Role;
 
 @RequiredArgsConstructor
 public enum ROLE implements Role {
 
-  COMPANY_MANAGER(Stream.of(MENU.COMPANY_MANAGEMENT).collect(Collectors.toSet()));
+  COMPANY_MANAGER;
 
   @Id
   @Getter
   private final String id = name();
 
-  @Transient
-  @Getter
-  @NonNull
-  private Set<Menu> menus;
 }
