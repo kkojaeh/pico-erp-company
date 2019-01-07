1dccb0973e3792e65ff805a9852a01a396ae4ec3
Ethan <kkojaeh@gmail.com>
2019-01-07 17:49:48 +0900

    테스트 데이터 및 소유사 정보 보충

--- impl/src/main/java/pico/erp/company/OwnerInitializer.java
+++ impl/src/main/java/pico/erp/company/OwnerInitializer.java
@@ -4,28 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import pico.erp.company.CompanyRequests.CreateRequest;
+import pico.erp.company.address.CompanyAddressRequests;
+import pico.erp.company.address.CompanyAddressService;
 import pico.erp.shared.ApplicationInitializer;
 
-@SuppressWarnings("SpringJavaAutowiringInspection")
+@SuppressWarnings({"SpringJavaInjectionPointsAutowiringInspection"})
 @Configuration
 public class OwnerInitializer implements ApplicationInitializer {
 
   @Autowired
   private CompanyService companyService;
 
+  @Autowired
+  private CompanyAddressService companyAddressService;
+
   @Override
   public void initialize() {
-    CreateRequest owner = owner();
+    CompanyRequests.CreateRequest owner = owner();
     if (!companyService.exists(owner.getId())) {
       companyService.create(owner);
+      CompanyAddressRequests.CreateRequest ownerAddress = ownerAddress();
+      companyAddressService.create(ownerAddress);
     }
   }
 
   @Bean
   @ConfigurationProperties("owner")
-  public CreateRequest owner() {
-    return new CreateRequest();
+  public CompanyRequests.CreateRequest owner() {
+    return new CompanyRequests.CreateRequest();
+  }
+
+  @Bean
+  @ConfigurationProperties("owner.address")
+  public CompanyAddressRequests.CreateRequest ownerAddress() {
+    return new CompanyAddressRequests.CreateRequest();
   }
 
 }
